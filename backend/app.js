a07878f7462aadad2ab348287fbec78998a98d7d
TanveerAhmadDev <tanveeera84@gmail.com>
2025-12-26 10:32:31 +0500

    Backend

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import ContactUs from "./Emails/ContactUs.js";
+import { sendMail } from "./SendMail.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is working on Vercel!" });
+});
+
+app.post("/sendemail", async (req, res) => {
+  const { email, message, name } = req.body;
+
+  if (!email || !message || !name) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Please Give Full Data" });
+  }
+
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ success: false, error: "Invalid email" });
+  }
+
+  const template = ContactUs({ email, message, name });
+
+  try {
+    await sendMail({ email, template, name });
+    return res.status(200).json({
+      success: true,
+      message: "Mail sent successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      error: err.message,
+      message: "Failed to send mail",
+    });
+  }
+});
+
+export default app;
